2aed356f3b93b355a765e0ad1cdf309f47700fa7
EvaSchulteis <eva.m.schulteis@gmail.com>
2026-07-12 06:48:22 -0700

    wrote MVP for crunchbase.py

--- src/ai_intelligence_platform/ingestion/crunchbase.py
+++ src/ai_intelligence_platform/ingestion/crunchbase.py
@@ -1,1 +1,4 @@
-raise NotImplementedError
+from ai_intelligence_platform.domain import Company
+
+def fetch_company_from_crunchbase(company_name: str) -> Company:
+    return Company(name=company_name)
