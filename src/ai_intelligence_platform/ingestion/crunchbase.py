d341000143de400c6f306ecaeabe3b1b9445e232
EvaSchulteis <eva.m.schulteis@gmail.com>
2026-07-17 06:30:50 -0700

    added requests dependency, added function get_crunchbase_response()

--- src/ai_intelligence_platform/ingestion/crunchbase.py
+++ src/ai_intelligence_platform/ingestion/crunchbase.py
@@ -1,15 +1,24 @@
+import json
+
 from ai_intelligence_platform.domain import Company
 
-MOCK_COMPANIES = {
-    "Anthropic": {
+MOCK_CRUNCHBASE_RESPONSES = {
+    "Anthropic": """
+    {
         "name": "Anthropic",
         "website": "https://anthropic.com",
-        "founded_year": 2021,
-    },
+        "founded_year": 2021
+    }
+    """,
 }
 
 def fetch_company_from_crunchbase(company_name: str) -> Company:
-    crunchbase_response = MOCK_COMPANIES[company_name]
+    crunchbase_response = get_crunchbase_response(company_name)
     return Company(
-        name=crunchbase_response["name"]
+        name=crunchbase_response["name"],
     )
+
+def get_crunchbase_response(company_name: str) -> dict:
+    crunchbase_response_text = MOCK_CRUNCHBASE_RESPONSES[company_name]
+    crunchbase_response = json.loads(crunchbase_response_text)
+    return crunchbase_response
